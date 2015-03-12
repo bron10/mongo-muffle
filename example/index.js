fbbb4e362d3b14ca9b34852612f7a7fb9c3944e7
Chetan Kantharia <chetan.kantharia@raweng.com>
2015-03-12 17:19:02 +0530

    Updated lib

--- example/index.js
+++ example/index.js
@@ -1,33 +1,41 @@
-var express = require('express')
-var app = express()
-var muffle = require('../')
+var express = require('express');
+var app     = express();
+var muffle  = require('../');
+
+// Constant
+var server = {
+	PORT : process.env.PORT || 3000,
+	IP   : process.env.IP || '127.0.0.1'
+}
+
 muffler = new muffle();
 
 muffler.connect({
   uri : "mongodb://localhost:27017"
 })
 
-app.get('/', function (req, res){
-	res.send('Hello World!');
-})
-app.get('/new', function (req, res){
-	new muffler.schema('car', {
-	'color':'',
-	'name' : [],
-	'modelno' : 0,
-	'features':{
-		"seats" : 0,
-		"abs" : false,
-		"hoods": false,
-		"engine":''
-	}
-	}).then(function(car){
-		res.send(car);
-	});
-})
+app.route('/')
+	 .get(function(req,res){
+	 	res.send('Hello Mongo!');
+	 });
+
+app.route('/new')
+	 .get(function (req, res){
+	 	new muffler.schema('car', {
+	 		'color':'',
+	 		'name' : [],
+	 		'modelno' : 0,
+	 		'features':{
+	 			'seats' : 0,
+	 			'abs' : false,
+	 			'hoods': false,
+	 			'engine':''
+	 		}
+	 	}).then(function(car){
+	 		res.send(car);
+	 	});
+	 })
 	
-var server = app.listen(3000, function () {
-  var host = server.address().address;
-  var port = server.address().port;
-  console.log('Example app listening at http://%s:%s', host, port)
-})
+app.listen(server.PORT, function () {
+  console.log('Example app listening at http://' + server.IP + ':' + server.PORT);
+});
